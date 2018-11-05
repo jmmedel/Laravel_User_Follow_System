6c36902081322faea835ee35c030818c0498e0a2
jm.kagaya@gmail.com <jm.kagaya@gmail.com>
2018-11-05 21:33:31 +0900

    Done
    done

---
+++ follow/app/User.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App;
+
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Overtrue\LaravelFollow\Traits\CanFollow;
+use Overtrue\LaravelFollow\Traits\CanBeFollowed;
+
+class User extends Authenticatable
+{
+    use Notifiable, CanFollow, CanBeFollowed;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name', 'email', 'password',
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password', 'remember_token',
+    ];
+}
